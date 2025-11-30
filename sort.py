50d3b84f45642d8027aa45adb935c989f13470eb
MukundC25 <officialmukundchavan@gmail.com>
2025-11-30 19:25:43 +0530

    insert

--- sort.py
+++ sort.py
@@ -3,6 +3,7 @@ print("Original:", numbers)
 numbers.sort(reverse=True)
 print("Sorted (descending):", numbers)'''
 
+##########
 
 #sorts based on length of string
 '''def fun(s):
@@ -13,6 +14,7 @@ print(strings)
 strings.sort(key=fun, reverse=True)
 print(strings)'''
 
+############
 
 #sorts in alphabetical order
 '''def fun(s):
@@ -23,6 +25,7 @@ print(strings)
 strings.sort()
 print(strings)'''
 
+############
 
 #sorts based on last character of string
 '''def fun(s):
@@ -32,6 +35,7 @@ print(strings)
 strings.sort(key=fun)
 print(strings)'''
 
+############
 
 #sorts based on name of student
 '''class student:
@@ -61,6 +65,7 @@ print('after sorting:')
 for s in students:
     print(s)'''
 
+############
 
 '''marks = [60, 30, 50, 20, 10, 40]
 print(marks)
@@ -68,7 +73,7 @@ sorted(marks)  # returns a new sorted list
 print(marks)
 print(sorted(marks))  # prints the new sorted list'''
 
-
+############
 
 '''def fun(n):
     if n>=0: #if positive
@@ -80,3 +85,101 @@ new_numbers = sorted(numbers, key=fun)
 print(numbers)
 print(new_numbers)'''
 
+############
+
+#Stability in sorting algorithms
+'''list = [(2, 'aaa'), (1, 'bbb'), (3, 'ccc'), (2, 'ddd'), (1, 'eee'), (3, 'fff')]
+print(list)
+list.sort()
+print(list)'''
+
+
+#Inplace sorting alogorithm
+
+############
+
+#bubble sort
+'''import random
+def bubbleSort(list):
+    for i in range(len(list)-1): #for i in range of length of list -1
+        for j in range(len(list)-i-1): #for comparing adjacent elements
+            if list[j]>list[j+1]: #if current element is greater than next element
+                #swap
+                list[j], list[j+1] = list[j+1], list[j]
+
+list = []
+for i in range(10):
+    list.append(random.randint(10,99)) #in random module randint fn generate random integer value between the given range
+print(list)
+bubbleSort(list)
+print(list)'''
+
+###########
+
+#iteration for sorted list 
+'''import random
+
+c=0
+def bubbleSort(list):
+    flag = False
+    for i in range(len(list)-1): #for i in range of length of list -1
+        global c
+        c=c+1
+        for j in range(len(list)-i-1): #for comparing adjacent elements
+            if list[j]>list[j+1]: #if current element is greater than next element
+                flag=True #if atleast one swap happens then we set flag to true
+                #swap
+                list[j], list[j+1] = list[j+1], list[j]
+        if flag == False:
+                break
+
+list = [10,20,30,40,50,60,70,80,90,100]
+print(list)
+bubbleSort(list)
+print(list)
+print("Number of iterations:", c)'''
+
+
+############
+
+#slection sort
+'''import random
+
+def selectionSort(list):
+    n = len(list)
+    for i in range(n-1):
+        min_i = 0
+        for j in range(1, n-i):
+            if list[j]>list[min_i]:
+                min_i = j
+        #swap
+        list[n-i-1],list[min_i] = list[min_i], list[n-i-1]
+
+list = []
+for i in range(10):
+    list.append(random.randint(10,99))
+print(list)
+selectionSort(list)
+print(list)'''
+
+############
+
+#insertion sort
+'''import random
+
+def insertionSort(list):
+    n = len(list)
+    for i in range(1, n):
+        x = list[i]
+        j = i-1
+        while j>=0 and x<list[j]:
+            list[j+1] = list[j]
+            j = j-1
+        list[j+1] = x
+
+list = []
+for i in range(10):
+    list.append(random.randint(10,99))
+print(list)
+insertionSort(list)
+print(list)'''
